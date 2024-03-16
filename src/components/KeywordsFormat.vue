831a7b725bd687b65ae7c15c9de45e4a464f729e
tampt <phamthanhtam020295@gmail.com>
2024-03-16 23:59:13 +0700

    Update layout

--- src/components/KeywordsFormat.vue
+++ src/components/KeywordsFormat.vue
@@ -1,27 +1,33 @@
 <template>
     <section class="my-8">
 
-        <div class="grid grid-cols-2 gap-8">
+        <div class="grid grid-cols-2 gap-10">
             <div class="space-y-4">
-                <h2 class="app-section-title">Keywords Format</h2>
-                <a-textarea v-model:value="keywordStr" placeholder="Enter keywords..." :rows="4" />
+                <div class="flex items-center justify-between">
+                    <h2 class="app-section-title">Keywords Format</h2>
+                    <a-button type="primary" @click="onRemoveDuplicate()">Remove duplicate</a-button>
+                </div>
+                <a-textarea v-model:value="wordStr" placeholder="Enter keywords..." :rows="4" ref="inputRef" />
+                <div>
+                    <a-button danger type="primary" @click="onClear">Clear</a-button>
+                </div>
                 <div class="flex gap-2 flex-wrap">
-                    <a-tag v-for="(word, index) of keywords" :color="dupMarked[word] ? dupMarked[word] : ''" closable
+                    <a-tag v-for="(word, index) of wordList" :color="dupMarked[word] ? dupMarked[word] : ''" closable
                         @close.prevent="onRemoveKeyword(index)" class="mr-0">
                         {{ word }}
                     </a-tag>
                 </div>
             </div>
             <div class="space-y-4">
-                <div class="text-right">
-                    <a-button type="primary" @click="onRemoveDuplicate()">Remove duplicate</a-button>
-                </div>
+                <h3 class="font-bold leading-8">
+                    Result:
+                </h3>
                 <div class="italic leading-5">
-                    <p> {{ result }}</p>
+                    <p> {{ keywordString }}</p>
                 </div>
-                <div v-if="result" class="flex items-center justify-between">
+                <div v-if="keywordString" class="flex items-center justify-between">
                     <a class="text-blue-500 inline-block cursor-pointer" @click="onCopy">{{ copyText }}</a>
-                    <div>Total: <b>{{ resultWordCount }}</b></div>
+                    <div>Total: <b>{{ keywordsCount }}</b></div>
                 </div>
 
             </div>
@@ -30,43 +36,54 @@
 </template>
 <script setup lang="ts">
 import Utils from '@/core/utils';
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
-const keywordStr = ref('')
-const result = ref('')
+const wordStr = ref('')
+const keywordString = ref('')
 const copyText = ref('Copy')
-const keywords = computed(() => keywordStr.value.trim().toLowerCase()
+const inputRef = ref()
+const wordList = computed(() => wordStr.value.trim().toLowerCase()
     .split(',').filter(word => !!word).map(word => word.trim())
 )
 
-const resultWordCount = computed(() => result.value.split(',').length)
+const keywordsCount = computed(() => keywordString.value.split(',').length)
 
 const dupMarked = computed(() => {
-    const duplicates = Utils.findDuplicate(keywords.value)
+    const duplicates = Utils.findDuplicate(wordList.value)
     return duplicates.reduce((entry, item) => ({
         ...entry,
         [item]: Utils.stringToColor(item)
     }), {} as { [key: string]: string })
 })
 
+onMounted(() => {
+    inputRef.value.focus()
+})
+
 const onRemoveKeyword = (index: number) => {
-    const _keywords = keywordStr.value.split(',').filter(word => !!word)
-    _keywords.splice(index, 1)
-    keywordStr.value = _keywords.join(',')
+    const _wordList = wordStr.value.split(',').filter(word => !!word)
+    _wordList.splice(index, 1)
+    wordStr.value = _wordList.join(',')
 }
 
 const onRemoveDuplicate = () => {
-    result.value = ''
+    keywordString.value = ''
     setTimeout(() => {
-        result.value = [...new Set(keywords.value)].join(', ')
+        keywordString.value = [...new Set(wordList.value)].join(', ')
     }, 500)
 }
 
 const onCopy = async () => {
-    await navigator.clipboard.writeText(result.value)
+    await navigator.clipboard.writeText(keywordString.value)
     copyText.value = 'Copied!'
     await new Promise((resolve) => setTimeout(resolve, 2000));
     copyText.value = 'Copy'
+}
+
+const onClear = () => {
+    wordStr.value = ''
+    keywordString.value = ''
+    inputRef.value.focus()
 }
 
 </script>
