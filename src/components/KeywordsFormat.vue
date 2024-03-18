10e6e96395c4fcb532f2daadeccbd544d2ef3170
tampt <phamthanhtam020295@gmail.com>
2024-03-18 23:51:37 +0700

    Update tool

--- src/components/KeywordsFormat.vue
+++ src/components/KeywordsFormat.vue
@@ -11,24 +11,27 @@
                 <div>
                     <a-button danger type="primary" @click="onClear">Clear</a-button>
                 </div>
-                <div class="flex gap-2 flex-wrap">
-                    <a-tag v-for="(word, index) of wordList" :color="dupMarked[word] ? dupMarked[word] : ''" closable
-                        @close.prevent="onRemoveKeyword(index)" class="mr-0">
-                        {{ word }}
-                    </a-tag>
-                </div>
+
             </div>
             <div class="space-y-4">
                 <h3 class="font-bold leading-8">
                     Result:
                 </h3>
-                <div class="italic leading-5">
-                    <p> {{ keywordString }}</p>
-                </div>
-                <div v-if="keywordString" class="flex items-center justify-between">
-                    <a class="text-blue-500 inline-block cursor-pointer" @click="onCopy">{{ copyText }}</a>
-                    <div>Total: <b>{{ keywordsCount }}</b></div>
-                </div>
+                <template v-if="keywords.length">
+                    <div class="flex gap-2 flex-wrap">
+                        <a-tag v-for="(word, index) of keywords" :color="dupMarked[word] ? dupMarked[word] : ''"
+                            closable @close.prevent="onRemoveKeyword(index)" class="mr-0">
+                            {{ word }}
+                        </a-tag>
+                    </div>
+                    <div class="italic leading-5">
+                        <p> {{ keywordStr }}</p>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <a class="text-blue-500 inline-block cursor-pointer" @click="onCopy">{{ copyText }}</a>
+                        <div>Total: <b>{{ keywordsCount }}</b></div>
+                    </div>
+                </template>
 
             </div>
         </div>
@@ -39,14 +42,15 @@ import Utils from '@/core/utils';
 import { computed, onMounted, ref } from 'vue';
 
 const wordStr = ref('')
-const keywordString = ref('')
+const keywords = ref<string[]>([])
 const copyText = ref('Copy')
 const inputRef = ref()
 const wordList = computed(() => wordStr.value.trim().toLowerCase()
     .split(',').filter(word => !!word).map(word => word.trim())
 )
 
-const keywordsCount = computed(() => keywordString.value.split(',').length)
+const keywordsCount = computed(() => keywords.value.length)
+const keywordStr = computed(() => keywords.value.join(', '))
 
 const dupMarked = computed(() => {
     const duplicates = Utils.findDuplicate(wordList.value)
@@ -61,20 +65,20 @@ onMounted(() => {
 })
 
 const onRemoveKeyword = (index: number) => {
-    const _wordList = wordStr.value.split(',').filter(word => !!word)
-    _wordList.splice(index, 1)
-    wordStr.value = _wordList.join(',')
+    const _keywords = [...keywords.value]
+    _keywords.splice(index, 1)
+    keywords.value = _keywords
 }
 
 const onRemoveDuplicate = () => {
-    keywordString.value = ''
+    keywords.value = []
     setTimeout(() => {
-        keywordString.value = [...new Set(wordList.value)].join(', ')
+        keywords.value = [...new Set(wordList.value)]
     }, 500)
 }
 
 const onCopy = async () => {
-    await navigator.clipboard.writeText(keywordString.value)
+    await navigator.clipboard.writeText(keywordStr.value)
     copyText.value = 'Copied!'
     await new Promise((resolve) => setTimeout(resolve, 2000));
     copyText.value = 'Copy'
@@ -82,7 +86,7 @@ const onCopy = async () => {
 
 const onClear = () => {
     wordStr.value = ''
-    keywordString.value = ''
+    keywords.value = []
     inputRef.value.focus()
 }
 
